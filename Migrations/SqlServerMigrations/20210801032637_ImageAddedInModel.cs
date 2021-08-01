da8d01aa7f3e955fa67d36d3aed297d38b50dd0d
GithubDjk <karkidipen59@gmail.com>
2021-08-01 13:27:17 +0545

    Image Added in the Voterlist

---
+++ Migrations/SqlServerMigrations/20210801032637_ImageAddedInModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VoterListApp.Migrations.ApplicationDb
+{
+    public partial class ImageAddedInModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Citizens",
+                type: "nvarchar(100)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Citizens");
+        }
+    }
+}
